ac0d6a13466e7603e34b8c3ee07c6f18f7a15988
liss <lissqqm@foxmail.com>
2026-08-12 15:13:32 +0800

    add sifive test device and cpu init

--- hw/riscv/vlt_host.c
+++ hw/riscv/vlt_host.c
@@ -13,17 +13,45 @@
 #include "qemu/osdep.h"
 #include "qemu/error-report.h"
 #include "qemu/log.h"
+#include "qemu/units.h"
+#include "qapi/error.h"
 
 /* qom/qdev */
 #include "hw/riscv/vlt_host.h"
 #include "hw/riscv/machines-qom.h"
+#include "hw/misc/sifive_test.h"
 
 /* riscv */
 #include "target/riscv/cpu.h"
 
-static void vl_host_board_init(MachineState *machine)
+/* system */
+#include "system/address-spaces.h"
+
+static const MemMapEntry vlt_host_map[] = {
+    [VLT_HOST_DEV_TEST] = {        0x0, 0x1000 },
+
+    [VLT_HOST_DEV_DRAM] = { 0x80000000, 0x0    }
+};
+
+static void vlt_host_board_init(MachineState *machine)
 {
-    (void)machine;
+    VltHostState *s = VLT_HOST_MACHINE(machine);
+    const MemMapEntry *memmap = vlt_host_map;
+
+    /* Initialize CPU */
+    info_report("cpu_type=%s", machine->cpu_type ? machine->cpu_type : " ");
+    object_initialize_child(OBJECT(machine), "cpus", &s->cpus, 
+                            TYPE_RISCV_HART_ARRAY);
+    object_property_set_str(OBJECT(&s->cpus), "cpu-type",
+                            machine->cpu_type, &error_abort);
+    sysbus_realize(SYS_BUS_DEVICE(&s->cpus), &error_fatal);
+
+    /* Create SiFive Test MMIO device */
+    sifive_test_create(memmap[VLT_HOST_DEV_TEST].base);
+
+
+    /* Register system main memory */
+    // [TODO]
 
     info_report("vlt_host board init, now void");
 }
@@ -31,21 +59,19 @@ static void vl_host_board_init(MachineState *machine)
 static void vlt_host_machine_instance_init(Object *obj)
 {
     (void)obj;
-
-    info_report("vlt_host instance init, now void");
+    qemu_log("vlt_host instance init, do nothing\n");
 }
 
 static void vlt_host_machine_class_init(ObjectClass *oc, const void *data)
 {
-    qemu_log("vlt_host class init");
-
     MachineClass *mc = MACHINE_CLASS(oc);
     mc->desc = "Minimal RISC-V SoC (CPU+RAM) for Verilator co-simulation";
-    mc->init = vl_host_board_init;
+    mc->init = vlt_host_board_init;
+    mc->default_cpu_type = TYPE_RISCV_CPU_MAX;
 }
 
 static const TypeInfo vlt_host_machine_type_info = {
-    .name       = MACHINE_TYPE_NAME("vlt_host"),
+    .name       = TYPE_VLT_HOST_MACHINE,
     .parent     = TYPE_MACHINE,
     .class_init = vlt_host_machine_class_init,
     .instance_init = vlt_host_machine_instance_init,
@@ -59,6 +85,3 @@ static void vlt_host_machine_init_register_types(void)
 }
 
 type_init(vlt_host_machine_init_register_types)
-
-
-
--- include/hw/riscv/vlt_host.h
+++ include/hw/riscv/vlt_host.h
@@ -4,21 +4,25 @@
 #include "hw/core/boards.h"
 #include "hw/riscv/riscv_hart.h"
 
+
 typedef struct VltHostState {
     /*< private >*/
-    DeviceState parent_obj;
+    MachineState parent_obj;
 
     /*< public >*/
     RISCVHartArrayState cpus;
 } VltHostState;
 
+#define TYPE_VLT_HOST_MACHINE   MACHINE_TYPE_NAME("vlt_host")
+DECLARE_INSTANCE_CHECKER(VltHostState, VLT_HOST_MACHINE, TYPE_VLT_HOST_MACHINE)
+
 enum {
-    VLT_HOST_DEV_DEBUG,
+    VLT_HOST_DEV_TEST,
     VLT_HOST_DEV_MROM,
     VLT_HOST_DEV_CLINT,
     VLT_HOST_DEV_HTIF,
     VLT_HOST_DEV_VL_BRG,
-    VL_HOST_DRAM
+    VLT_HOST_DEV_DRAM
 };
 
 #endif
